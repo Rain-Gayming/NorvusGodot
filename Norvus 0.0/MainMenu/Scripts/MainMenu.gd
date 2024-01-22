6d7b6163b3b39f868f8e378bd191e8d40553197e
Rain-Gayming <raingayming@gmail.com>
2024-01-22 13:19:44 +0000

    Settings Menu 1.0
    + Settings Menu Base

--- Norvus 0.0/MainMenu/Scripts/MainMenu.gd
+++ Norvus 0.0/MainMenu/Scripts/MainMenu.gd
@@ -6,30 +6,52 @@ extends Node
 @export var newGameMenu : Control
 @export var settingsMenu : Control
 
+@export_category("Settings Menu")
+@export var settingsTabs : TabContainer
+
+enum mainMenuTypes{
+	_continue,
+	_load,
+	_newGame,
+	_settings
+}
+
 func _ready():
-	_CloseMenus();
+	_CloseMenus()
 
 func _ContinueButtonPressed():
-	_CloseMenus();
-	continueMenu.visible = true;
+	_OpenMenu(mainMenuTypes._continue)
 
 func _LoadButtonPressed():
-	_CloseMenus();
-	loadMenu.visible = true;
+	_OpenMenu(mainMenuTypes._load)
 
 func _NewGameButtonPressed():
-	_CloseMenus();
-	newGameMenu.visible = true;
+	_OpenMenu(mainMenuTypes._newGame)
 
 func _SettingsButtonPressed():
-	_CloseMenus();
-	settingsMenu.visible = true;
+	_OpenMenu(mainMenuTypes._settings)
 
 func _ExitButtonPressed():
-	get_tree().quit();
+	get_tree().quit()
 
 func _CloseMenus():
-	continueMenu.visible = false;
-	loadMenu.visible = false;
-	newGameMenu.visible = false;
-	settingsMenu.visible = false;
+	continueMenu.visible = false
+	loadMenu.visible = false
+	newGameMenu.visible = false
+	settingsMenu.visible = false
+
+func _OpenMenu(menu : mainMenuTypes):
+	_CloseMenus()
+	if menu == mainMenuTypes._continue:
+		continueMenu.visible = true
+	elif menu == mainMenuTypes._load:
+		loadMenu.visible = true
+	elif menu == mainMenuTypes._newGame:
+		newGameMenu.visible = true
+	elif menu == mainMenuTypes._settings:
+		settingsMenu.visible = true
+
+func _OnSettingsTabClicked(tab):
+	if(tab == 4):
+		settingsTabs.current_tab = 0;
+		_CloseMenus();
